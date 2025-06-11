03f3b0db246b4cab6063a1ed000e036a5e058b71
Paulo Beldel Filho <beldelfilho@gmail.com>
2025-06-11 01:08:17 -0300

    delete exercicios duplicados

--- aula02/praticas/livro.py
+++
@@ -1,8 +0,0 @@
-class Livro:
-    def __init__(self, titulo, autor):
-        self.titulo = titulo
-        self.autor = autor
-
-    def exibir_informacoes(self):
-        print(f'Título: {self.titulo}, Autor: {self.autor}')
-
